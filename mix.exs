1f77911f73f138fd5857d77a6cad4e8fa5438b92
Amit Bashan <amitxbashan@gmail.com>
2026-02-03 08:12:29 +0200

    feat: add package metadata

--- mix.exs
+++ mix.exs
@@ -7,21 +7,39 @@ defmodule TextFSM.MixProject do
       version: "0.1.0",
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: description(),
+      package: package(),
+      deps: deps(),
+      name: "TextFSM",
+      source_url: "https://github.com/amitbashan/textfsm"
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:nimble_parsec, "~> 1.4"}
+    ]
+  end
+
+  defp description() do
+    """
+    TextFSM is a template-based state machine designed to parse semi-structured text (such as CLI output) into structured data.
+
+    This is an Elixir implementation of the original TextFSM written in Python.
+    """
+  end
+
+  defp package() do
+    [
+      files: ~w(lib .formatter.exs mix.exs README* LICENSE),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/amitbashan/textfsm"}
     ]
   end
 end
