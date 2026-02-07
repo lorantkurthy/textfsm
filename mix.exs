15ef5cdeba1dd8c27af5b2c2397157f0ba3d571f
Amit Bashan <amitxbashan@gmail.com>
2026-02-07 02:54:30 +0200

    feat: bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TextFSM.MixProject do
   def project do
     [
       app: :textfsm,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
       description: description(),
